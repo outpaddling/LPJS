22582059bc4f05603bfa77708a4b4aca8db135e8
Jason Bacon <bacon@moray.acadix.biz>
2021-09-30 12:00:11 -0500

    Add lpjs-compd stub (compute node daemon) Install daemons to sbin Add lpjs-compd.c to repo

---
+++ lpjs-compd.c
@@ -0,0 +1,19 @@
+/***************************************************************************
+ *  Description:
+ *      LPJS compute node daemon.  Checks in with lpfs-dispatchd to signal
+ *      that node is up and starts computational processes on compute nodes.
+ *
+ *  History: 
+ *  Date        Name        Modification
+ *  2021-09-30  Jason Bacon Begin
+ ***************************************************************************/
+
+#include <stdio.h>
+#include <sysexits.h>
+
+int     main(int argc,char *argv[])
+
+{
+    return EX_OK;
+}
+
