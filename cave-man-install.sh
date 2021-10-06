6fd5626e8fa7ec090e4f2941f23f1e49d0aa1fb4
Jason Bacon <bacon@NetBSD.org>
2021-10-06 08:55:54 -0500

    Add caveman-install.sh to help find munge

---
+++ cave-man-install.sh
@@ -0,0 +1,42 @@
+#!/bin/sh -e
+
+##########################################################################
+#
+#   Perform a cave-man install for development and testing purposes.
+#   For production use, this software should be installed via a package
+#   manager such as Debian packages, FreeBSD ports, MacPorts, pkgsrc, etc.
+#       
+#   History:
+#   Date        Name        Modification
+#   2021-07-12  Jason Bacon Begin
+##########################################################################
+
+# Default to ../local if PREFIX is not set
+: ${PREFIX:=../local}
+
+# Need separate LOCALBASE to find munge installed by FreeBSD ports or pkgsrc
+if [ -z $LOCALBASE ]; then
+    for d in /usr/local ~/Pkgsrc/pkg /usr/pkg /opt/pkg; do
+	if [ -e $d ]; then
+	    break;
+	fi
+    done
+fi
+LOCALBASE=$d
+printf "LOCALBASE = $LOCALBASE  PREFIX = $PREFIX\n"
+export PREFIX LOCALBASE
+
+# OS-dependent tricks
+# Set rpath to avoid picking up libs installed by package managers in
+# /usr/local/lib, etc.
+case $(uname) in
+*)
+    export CFLAGS="-Wall -g -O"
+    rp=$(realpath $PREFIX/lib)
+    rl=$(realpath $LOCALBASE/lib)
+    export LDFLAGS="-L$rp -L$rl -Wl,-rpath,$rp:$rl:/usr/lib:/lib"
+    ;;
+
+esac
+
+make clean install
