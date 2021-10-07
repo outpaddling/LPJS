aa4bb8492bafd2b292733b039b84a751f007cc85
Jason Bacon <bacon@moray.acadix.biz>
2021-10-07 12:56:12 -0500

    Make generated scripts executable and run from CWD

--- lpjs-submit.c
+++ lpjs-submit.c
@@ -88,7 +88,7 @@ int     main (int argc, char *argv[])
     sscanf(buff, "%s %u", host, &cores);
     str_argv_cat(remote_cmd, argv, 1, LPJS_CMD_MAX + 1);
     puts(remote_cmd);
-    snprintf(cmd, LPJS_CMD_MAX + 150, "ssh %s lpjs-chaperone %s\n",
+    snprintf(cmd, LPJS_CMD_MAX + 150, "ssh %s lpjs-chaperone ./%s\n",
 	     host, remote_cmd);
     puts(cmd);
     system(cmd);
