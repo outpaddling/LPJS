d1a933a98b261464d240d47a268e2c89e8eb7025
Jason W. Bacon <bacon4000@gmail.com>
2024-03-17 15:44:26 -0500

    Successfully test file transfer of results

--- moray-test.sh
+++ moray-test.sh
@@ -5,12 +5,12 @@ rm -f lpjs-job*
 ../local/sbin/lpjs-reset-queue
 ./submit test-script-3-job.lpjs
 count=0
-set -x
 while [ $count -lt 3 ]; do
     ./nodes
     sleep 2
     count=$(($count + 1))
 done
+set -x
 cat lpjs-job-3-test-script-3-job.lpjs.stdout
 cat lpjs-job-3-test-script-3-job.lpjs.stdout
 
