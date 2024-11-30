b2e75255cf25c6273ff34d11c31d1eaf280d3e6c
Jason W. Bacon <bacon4000@gmail.com>
2024-11-30 08:05:37 -0600

    Use argv[0] for program name in --version

--- lpjs.c
+++ lpjs.c
@@ -54,7 +54,7 @@ int     main(int argc,char *argv[])
     
     if ( (argc == 2) && (strcmp(argv[1],"--version") == 0) )
     {
-	printf("lpjs %s\n", VERSION);
+	printf("%s %s\n", argv[0], VERSION);
 	return EX_OK;
     }
     else if ( argc < 2 )
