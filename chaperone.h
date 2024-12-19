2e2f0ec9871cb8c4d782535cd9b038d7e4aa2411
Jason W. Bacon <bacon4000@gmail.com>
2024-12-19 17:04:17 -0600

    Add RSS limit via rctl on FreeBSD

---
+++ chaperone.h
@@ -0,0 +1,16 @@
+#ifndef __H_
+#define __H_
+
+#ifdef  __cplusplus
+extern "C" {
+#endif
+
+#define LPJS_RCTL_RULE_MAX  128
+
+#ifdef  __cplusplus
+}
+#endif
+
+#include "chaperone-protos.h"
+
+#endif  // #ifndef __H_
